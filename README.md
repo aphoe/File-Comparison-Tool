5c21cebde29a08ec480604cf66a941b7da2ef4f3
afolabi legunsen <afolabi@gettonote.com>
2025-07-25 14:31:13 +0100

    feat: Add favicon image
    Updated the README file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# file-comparison
-File Comparison Tool
+# File Comparison Tool
+### By [Afolabi Legunsen](https://aphoe.com)
+Do a line-by-line comparison of two code files. 
+
+It ignores empty lines, lines with only an opening or closing curly brace, and comments.
+
+## Favicon attribution
+Image used as icon created by [Muhammad Fajar Estu Nugroho](https://thenounproject.com/creator/felaboratory/) from Noun Project
+
+## License
+MIT License
--- index.php
+++ index.php
@@ -122,6 +122,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File Comparison Tool</title>
+    <link rel="icon" type="image/png" sizes="256x256" href="./assets/images/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
